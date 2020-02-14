6203ae4c2323dc1bbd1fd8d27df42eb1a1898a98
Levi Almeida <levialmeida1994@hotmail.com>
2020-02-14 16:29:11 -0300

    ver banco

--- Interface/script.js
+++ Interface/script.js
@@ -1,7 +1,77 @@
-function horario(){
+var btn= document.querySelectorAll(".tecla")
+var display= document.querySelectorAll(".display")
+var btn1 = document.getElementById('btn1')
+var btn2 = document.getElementById('btn2')
+var btn3 = document.getElementById('btn3')
+var btn4 = document.getElementById('btn4')
+    function horario(){
     var horario = document.getElementById('horario')
     var data = new Date()
     var horas = data.getHours()
     var minutos = data.getMinutes()
-    horario.innerHTML= ` ${horas} : ${minutos}`
-}
+    horario.innerHTML= `<strong> ${horas} : ${minutos}</strong>`
+    }
+
+    function muda(event)
+    {
+        btn1.innerHTML= btn2.innerHTML
+        btn2.innerHTML= btn3.innerHTML
+        btn3.innerHTML= btn4.innerHTML
+        btn4.innerHTML=event.target.innerHTML       
+    }
+
+    for(let i=0;i<btn.length;i++)
+    {
+        btn[i].addEventListener('click',muda,false)
+    }
+   
+
+    function saida(){
+    if(btn1.innerHTML==""||btn2.innerHTML==""||btn3.innerHTML==""||btn4.innerHTML==""){
+        alert("Preencha todos Numeros!")
+    }else{
+    alert(`Saida do Numero: ${btn1.innerHTML+btn2.innerHTML+btn3.innerHTML+btn4.innerHTML}`)
+    btn1.innerHTML= ""
+    btn2.innerHTML= ""
+    btn3.innerHTML= ""
+    btn4.innerHTML= ""
+        } 
+    }
+    function entrada(){
+    if(btn1.innerHTML==""||btn2.innerHTML==""||btn3.innerHTML==""||btn4.innerHTML==""){
+        alert("Preencha todos Numeros!")
+    }else{
+    alert(`Entrada do Numero: ${btn1.innerHTML+btn2.innerHTML+btn3.innerHTML+btn4.innerHTML}`)
+    btn1.innerHTML= ""
+    btn2.innerHTML= ""
+    btn3.innerHTML= ""
+    btn4.innerHTML= ""
+        }   
+    }
+    function delet(){
+        for(let i=0;i<display.length;i++)
+        {   
+            if(display[i].innerHTML!=""){
+            display[i].innerHTML!=""
+            }
+        }
+        /*
+        if(btn4.innerHTML!=""){
+            btn4.innerHTML=""
+            return
+        }
+        if(btn4.innerHTML==""){
+            btn3.innerHTML=""
+            btn4.innerHTML==null
+            return
+        }
+        if(btn3.innerHTML==""){
+            btn2.innerHTML=""
+            return
+        }
+        if(btn2.innerHTML==""){
+            btn1.innerHTML=""
+            return
+        }
+        */
+    }
